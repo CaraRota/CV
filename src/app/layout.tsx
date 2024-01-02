1932408d03af4a44bc1a3c0d85354cbf144c6f4e
CaraRota <canalsdigital@gmail.com>
2024-01-02 11:37:45 -0300

    Remove background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]">
+        {/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"> */}
           {children}
-        </div>
+        {/* </div> */}
       </body>
       <Analytics />
     </html>
