aedf31297b7bf5bea29d2f28cd9be92267de27ea
CaraRota <canalsdigital@gmail.com>
2024-01-11 21:12:26 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import GreenDot from "@/components/ui/greenDot";
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
-  description: RESUME_DATA.summary,
+  description: `${RESUME_DATA.summary}`,
 };
 
 export default function Page() {
